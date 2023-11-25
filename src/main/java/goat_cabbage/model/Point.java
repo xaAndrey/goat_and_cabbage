4e1c12506bfd766d0224dab4688c3d63e9cd3631
andreypisarev <xaandreynx@gmail.com>
2023-11-25 18:32:05 +0300

    [feat]: add Point class

---
+++ src/main/java/goat_cabbage/model/Point.java
@@ -0,0 +1,88 @@
+package goat_cabbage.model;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Objects;
+
+/**
+ * Точка-координат на игровом поле.
+ */
+public class Point {
+
+    /**
+     * Координата X.
+     */
+    private final int x;
+
+    /**
+     * Координата Y.
+     */
+    private final int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    /**
+     * Получить координату X.
+     * @return координата X.
+     */
+    public int getX() {
+        return this.x;
+    }
+
+    /**
+     * Получить координату Y.
+     * @return координата Y.
+     */
+    public int getY() {
+        return y;
+    }
+
+    /**
+     * Получить новую точку с заданным смещением в заданном направлении
+     * @param direction направление
+     * @param delta смещение
+     * @return новая точка с заданным смещением в заданном направлении
+     */
+    public Point to(@NotNull Direction direction, int delta) {
+        int newX = x;
+        int newY = y;
+        switch (direction) {
+            case NORTH:
+                newY -= delta;
+                break;
+            case SOUTH:
+                newY += delta;
+                break;
+            case EAST:
+                newX += delta;
+            case WEST:
+                newX -= delta;
+                break;
+        }
+        return new Point(newX, newY);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Point point = (Point) o;
+        return x == point.x && y == point.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
+
+    @Override
+    public String toString() {
+        return "Point{" +
+                "x" + x +
+                ", y=" + y +
+                '}';
+    }
+}
