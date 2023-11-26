cf8fa432f40e8a471c4fad0995dd77010155714c
andreypisarev <xaandreynx@gmail.com>
2023-11-26 14:37:10 +0300

    [feat]: add Game Widgets Utils class

---
+++ src/main/java/goat_cabbage/ui/utils/GameWidgetsUtils.java
@@ -0,0 +1,14 @@
+package goat_cabbage.ui.utils;
+
+import java.awt.*;
+
+public class GameWidgetsUtils {
+    private final static double MEDIUM_CHARGE_COEFFICIENT = 0.7;
+    private final static double LOW_CHARGE_COEFFICIENT = 0.3;
+    private final static int EMPTY_CHARGE_VALUE = 0;
+
+    public static Color numberMovesTextColor(int numberMoves) {
+        Color chargeTextColor = Color.GREEN;
+        return chargeTextColor;
+    }
+}
